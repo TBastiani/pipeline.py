c11e790417737b61142a174f8cedee89443c89cc
Thomas Bastiani <thom@codehawks.eu>
2017-12-18 17:43:50 +0000

    Add MultiStage example

---
+++ multi_stage_example.py
@@ -0,0 +1,33 @@
+from multi_stage import MultiStage
+from stage import Stage
+from pipeline import Pipeline
+import time
+
+class PassStage(MultiStage):
+    def process(self, packet):
+        time.sleep(1)
+        self.send(packet)
+
+class PrintStage(Stage):
+    def process(self, value):
+        print(value)
+
+# Setup pipeline
+pipe = Pipeline()
+
+multi = PassStage('multi', 10, 4)
+pipe.addStage(multi)
+
+printer = PrintStage('printer', 10)
+pipe.addStage(printer)
+multi.link(printer)
+
+# Start pipeline
+pipe.start()
+
+# Feed pipeline
+for i in range(100):
+    pipe.put(i)
+
+# Stop pipeline
+pipe.stop()
