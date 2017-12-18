e6c7466192e0fc39700a3061457521c6276dd683
Thomas Bastiani <thom@codehawks.eu>
2017-12-18 17:37:53 +0000

    Add Pipeline usage example

---
+++ pipeline_example.py
@@ -0,0 +1,30 @@
+from stage import Stage
+from pipeline import Pipeline
+
+class PowerStage(Stage):
+    def process(self, value):
+        self.send(value * value)
+
+class PrintStage(Stage):
+    def process(self, packet):
+        print(packet)
+
+# Setup pipeline
+pipe = Pipeline()
+
+pow = PowerStage('pow', 10)
+pipe.addStage(pow)
+
+printer = PrintStage('printer', 10)
+pipe.addStage(printer)
+pow.link(printer)
+
+# Start pipeline
+pipe.start()
+
+# Feed pipeline
+for i in range(1000):
+    pipe.put(i)
+
+# Stop pipeline
+pipe.stop()
